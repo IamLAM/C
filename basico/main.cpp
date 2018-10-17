8e1621f9894b7ae73ad01aa7903fb82a57db8b38
IamLAM <ingeniero@luismonge.com.mx>
2018-10-17 11:49:46 -0700

    Secuencias de escape

--- basico/main.cpp
+++ basico/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
 using namespace std;
+//evita que se agregue std::cout en cada salida
 
 int main()
 {
+    /*escapes
+
+    \n  \b \r  \t
+
+    */
+
     cout << "Buenos dias!" << endl;
+    cout << "Buenos dias!\n" <<"Hola"<< endl;
+    cout << "Buenos dias!\b" << endl;
+    cout << "Buenos dias!\t"  <<"Hola"<< endl;
+    cout << "Buenos dias!" << endl;
+    cout << "Hasta luego!\r"  <<"Nos vemos"<< endl;
     return 0;
 }
