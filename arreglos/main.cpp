4947f2e3e18f4c09b8ad81dbf50eb9d82f19ee21
IamLAM <ingeniero@luismonge.com.mx>
2018-10-26 00:50:59 -0700

    Declarando una matriz y desplegando su valor

--- arreglos/main.cpp
+++ arreglos/main.cpp
@@ -7,8 +7,8 @@ int main()
     cout << "Trabajando con arreglos" << endl;
     //declaración
 
-    int a[]={1,2,3,4};
-    int b[5];
+    int a[]= {1,2,3,4};
+    int b[4], c[4];
     int i=0;
 
     while(i<4)
@@ -17,6 +17,34 @@ int main()
 
         cout<<"Arreglo: "<<b[i]<<endl;
         i++;
+    }
+//declarando matrices
+    int filas=2, columnas=2;
+    int ma[filas][columnas];
+    int n=2;
+
+    for(int i=0; i<=1; i++)
+    {
+        for(int j=0; j<=1; j++)
+        {
+            n=n*2;
+            ma[i][j]=n;
+        }
+
+
+
+    }
+
+    for(int i=0; i<=1; i++)
+    {
+        for(int j=0; j<=1; j++)
+        {
+
+            cout<<"Desplegando matriz "<<"|F: "<<i<<"|C: "<<j<< " |Valor:" <<ma[i][j]<<endl;
+        }
+
+
+
     }
 
 
