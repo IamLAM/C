d9248ee6aed7ab79658660192f92f451ef9ed0b4
IamLAM <ingeniero@luismonge.com.mx>
2018-10-25 23:33:53 -0700

    Declarando switch para comparaciones multiples

--- condiciones/main.cpp
+++ condiciones/main.cpp
@@ -5,13 +5,35 @@ using namespace std;
 int main()
 {
     cout << "Condiciones" << endl;
-    int a=2,b=5;
+    int a=2,b=5,c;
 
     if(a>5)
-            cout<<"Si es mayor: "<<a<<endl;
+        cout<<"Si es mayor: "<<a<<endl;
     else
-            cout<<"Lo siento es menor: "<<a<<endl;
+        cout<<"Lo siento es menor: "<<a<<endl;
 
+
+
+    /*comparando con diversos valores*/
+    cout<<"Proporciona un valor"<<endl;
+    cin>>c;
+    switch(c)
+    {
+    case 1:
+        cout<<"Es diferente a 1 "<<a<<endl;
+        break;
+
+    case 2:
+        cout<<"Es diferente a 2 "<<a<<endl;
+        break;
+
+    case 3:
+        cout<<"Es diferente a 3 "<<a<<endl;
+        break;
+    default:
+        cout<<"El valor no fue encontrado";
+
+    }
 
 
 
