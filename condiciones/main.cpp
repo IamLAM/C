6f48ced895ca12891392065ce343b3f9b4941e19
IamLAM <ingeniero@luismonge.com.mx>
2018-10-25 23:29:55 -0700

    Declarando if condicional

--- condiciones/main.cpp
+++ condiciones/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Condiciones" << endl;
+    int a=2,b=5;
+
+    if(a>5)
+            cout<<"Si es mayor: "<<a<<endl;
+    else
+            cout<<"Lo siento es menor: "<<a<<endl;
+
+
+
+
     return 0;
 }
