d8462e39a5898bed30a3acbb873544329179038d
IamLAM <ingeniero@luismonge.com.mx>
2018-10-27 15:09:51 -0700

    Accediendo a dirección de memoria y copia a puntero

--- punteros/main.cpp
+++ punteros/main.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main()
 {
     cout << "Trabajando con punteros y referencias" << endl;
+
+    //accederemos a la dirección de memoria de una variable
+    int a=10;
+    int *p=&a;
+    cout <<"Contenido de la variable"<<a<<endl;
+    cout <<"Dirección de memoria de la variable"<<&a<<endl;
+    cout <<"Contenido de memoria del puntero"<<p<<endl;
+    cout <<"Dirección de memoria del puntero"<<&p<<endl;
+
     return 0;
 }
