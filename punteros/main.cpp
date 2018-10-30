7bd4d5ff3cac947ec759ef85ee99d5da464082fb
IamLAM <ingeniero@luismonge.com.mx>
2018-10-29 17:52:39 -0700

    Ejercicio numero primo con punteros

--- punteros/main.cpp
+++ punteros/main.cpp
@@ -28,25 +28,50 @@ int main()
     cout <<"Contenido de la referencia"<<r<<endl;
     cout <<"Dirección de memoria de la referencia"<<&r<<endl;
 
+    /*
+        cout<<"Trabajando con punteros: par o impar"<<endl;
 
-    cout<<"Trabajando con punteros: par o impar"<<endl;
+        int numero,*pn;
 
-    int numero,*pn;
+        cout<<"Cual es tu numero:"; cin>>numero;
+        pn=&numero;
 
-    cout<<"Cual es tu numero:"; cin>>numero;
-    pn=&numero;
+        if(*pn%2==0)
+            {cout<<"Es numero "<<*pn<<"es par"<<endl;
+             cout<<"Posición de memoria"<<pn<<endl;
+            }
+        else
+            {cout<<"Es numero "<<*pn<<"es impar"<<endl;
+             cout<<"Posición de memoria"<<pn<<endl;
 
-    if(*pn%2==0)
-        {cout<<"Es numero "<<*pn<<"es par"<<endl;
-         cout<<"Posición de memoria"<<pn<<endl;
+            }
+    */
+    cls();
+    int numero,*pn,c=0;
+    char salir='n';
+    while(salir!='y')
+    {
+
+        cout<<"Trabajando con numeros primos"<<endl;
+        cout<<"Proporciona un numero:"<<endl;
+        cin>>numero;
+        pn=&numero;
+
+        for(int i=1; i<*pn; i++)
+        {
+
+            if(*pn%i==0)
+                c++;
         }
-    else
-        {cout<<"Es numero "<<*pn<<"es impar"<<endl;
-         cout<<"Posición de memoria"<<pn<<endl;
 
-        }
+        if(c==2)
+            cout<<"es un numero primo"<<endl;
+        else
+            cout<<"no es un numero primo";
 
-
+        cout<<"deseas salir?(y/n):";
+        cin>>salir;
+    }
     getch();
 
     return 0;
