d234fb31cdd09dae171390bc301c1e18e06f67f6
IamLAM <ingeniero@luismonge.com.mx>
2018-10-31 01:03:03 -0700

    Creación de función para arreglo dinamico

---
+++ punteros_arreglos/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int n, *nc;
+void obtenerNota();
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+void obtenerNota(){
+
+
+cout<<"Proporciona el numero de notas para almacenar"<<endl;
+cin>n;
+nc=new int[n];
+
+
+}
