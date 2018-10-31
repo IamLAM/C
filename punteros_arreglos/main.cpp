407575284eac7a1572448c00b8911731a4ce1718
IamLAM <ingeniero@luismonge.com.mx>
2018-10-31 01:14:37 -0700

    se almacenan calificaciones dinámicas y se despliegan

--- punteros_arreglos/main.cpp
+++ punteros_arreglos/main.cpp
@@ -3,21 +3,42 @@
 
 using namespace std;
 
-int n, *nc;
+int n, *pc;
 void obtenerNota();
-
+void desplegarNota();
 int main()
 {
-    cout << "Hello world!" << endl;
+    obtenerNota();
+    desplegarNota();
+
+
+
     return 0;
 }
 
 void obtenerNota(){
-
+int i=0;
 
 cout<<"Proporciona el numero de notas para almacenar"<<endl;
-cin>n;
-nc=new int[n];
+cin>>n;
+pc=new int[n];
+
+while(i<n){
+
+    cout<<"indica tu calificación["<<i<<"]"<<":";
+    cin>>pc[i];
+    i++;
+    }
+
+}
 
 
+void desplegarNota(){
+int i=0;
+cout<<"Mostrar notas del alumno"<<endl;
+while(i<n){
+
+    cout<<"Nota ["<<i<<"]:"<<pc[i]<<endl;
+    i++;
+}
 }
