f6586eb8a679644717a47187330324f4495cd93b
IamLAM <ingeniero@luismonge.com.mx>
2018-11-02 22:47:40 -0700

    Intercambio de valores por medio de punteros

--- punteros_intercambio/main.cpp
+++ punteros_intercambio/main.cpp
@@ -7,9 +7,13 @@ void cambiar(int *,int *);
 
 int main()
 {   int a=5,b=10;
-
-
+//valores iniciales
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
     cambiar(&a,&b);
+//valores intercambiados por medio de punteros
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
     return 0;
 }
 
