9c1963ba66224734af4c3ea19b9b128887c20064
IamLAM <ingeniero@luismonge.com.mx>
2018-11-02 22:52:13 -0700

    Se agrego toma de decisiones para enteros o arreglos

--- punteros_intercambio/main.cpp
+++ punteros_intercambio/main.cpp
@@ -7,13 +7,31 @@ void cambiar(int *,int *);
 
 int main()
 {   int a=5,b=10;
+    int op;
+
+cout<<"Que deseas intercambiar (1)valores enteros (2)arreglos"
+cin>>op;
 //valores iniciales
-    cout<<"a="<<a<<endl;
+switch(op){
+
+case 1:
+        cout<<"a="<<a<<endl;
     cout<<"b="<<b<<endl;
     cambiar(&a,&b);
 //valores intercambiados por medio de punteros
     cout<<"a="<<a<<endl;
     cout<<"b="<<b<<endl;
+break;
+case 2:
+break;
+
+default:
+    cout<<"No existe la opción seleccionada";
+
+
+
+}
+
     return 0;
 }
 
