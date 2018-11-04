3e6e80cacfc2bb0b3f8c573ca01451e7ad8bc8f0
IamLAM <ingeniero@luismonge.com.mx>
2018-11-03 22:12:59 -0800

    Creación de estructura tipo alumno y registro de valores

--- punteros_estructuras/main.cpp
+++ punteros_estructuras/main.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
+struct alumno
+{
+    char nombre[20];
+    int semestre;
+
+} alum,*puntero_alumno=&alum;
+
+
+void registrarAlumno();
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    registrarAlumno();
     return 0;
+}
+
+
+void registrarAlumno()
+{
+
+    cout<<"Cual es tu nombre?";
+    cin.getline(puntero_alumno->nombre,30,'\n');
+    cout<<"En que semestre estas?";
+    cin>>puntero_alumno->semestre;
+
 }
