5d6b77cccca04f02252816d52f17d307b72bc723
IamLAM <ingeniero@luismonge.com.mx>
2018-11-04 23:08:30 -0800

    Declaración de estructura de pila y función base

--- pilas/main.cpp
+++ pilas/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
+#include<stdlib.h>
 using namespace std;
+
+struct Nodo{
+int valor;
+Nodo *siguiente;
+
+};
+
+void insertarPila(Nodo *&,int);
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //El primero en entrar es el ultimo en salir
+    //El ultimo que entra es el primero que sale
+
+    cout << "Trabajando con pilas" << endl;
+
+
+
     return 0;
+}
+
+void insertarPila(Nodo *&pila,int v){
+Nodo *insPila=new Nodo();
+
+
 }
