159973e774eecb850eb08d21d330edfc88d576be
IamLAM <ingeniero@luismonge.com.mx>
2018-11-04 23:15:28 -0800

    Función insertar pila creada

--- pilas/main.cpp
+++ pilas/main.cpp
@@ -2,9 +2,10 @@
 #include<stdlib.h>
 using namespace std;
 
-struct Nodo{
-int valor;
-Nodo *siguiente;
+struct Nodo
+{
+    int valor;
+    Nodo *siguiente;
 
 };
 
@@ -15,16 +16,29 @@ int main()
 {
     //El primero en entrar es el ultimo en salir
     //El ultimo que entra es el primero que sale
+    int cantidad,i=0,v;
 
+    Nodo *pila=NULL;
     cout << "Trabajando con pilas" << endl;
+    cout<<"Proporciona la cantidad de numeros a introducir en la pila"<<endl;
+    cin>>cantidad;
 
+    while(i<cantidad){
+        cout<<"Dame el valor["<<i<<"]:";
+        cin>>v;
+
+    }
 
 
     return 0;
 }
 
-void insertarPila(Nodo *&pila,int v){
-Nodo *insPila=new Nodo();
+void insertarPila(Nodo *&pila,int v)
+{
+    Nodo *insPila=new Nodo();
+    insPila->valor=v;
+    insPila->siguiente=pila;
+    pila=insPila;
 
 
 }
