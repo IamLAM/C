55f96c247585aa680978ef68d1a97eb92e1c9221
IamLAM <ingeniero@luismonge.com.mx>
2018-11-05 22:37:12 -0800

    Se modifico el menu de opciones para que el usuario decida cuando salir

--- pilas/main.cpp
+++ pilas/main.cpp
@@ -18,13 +18,14 @@ int main()
 {
     //El primero en entrar es el ultimo en salir
     //El ultimo que entra es el primero que sale
-    int cantidad,i=0,v;
+    int cantidad,i=0,v,op=1;
     Nodo *pila=NULL;
-
+   while(op!=4){
     cout<<"Selecciona la opción que deseas realizar:"<<endl;
     cout<<"(1)Insertar valor a pila"<<endl;
     cout<<"(2)Mostrar valor a pila"<<endl;
     cout<<"(3)Eliminar valor a pila"<<endl;
+    cout<<"(4)Salir"<<endl;
     cin>>op;
     switch(op)
     {
@@ -45,13 +46,39 @@ int main()
         break;
 
     case 3:
-        eliminarPila(pila);
+
+        if(pila==NULL)
+        {
+            cout<<"Una disculpa, los datos no pueden ser eliminados:pila vacia"<<endl;
+
+        }
+        else
+        {   cout<<"se eliminara dato";
+/*
+            while(pila!=NULL)
+            {
+                eliminarPila(pila,v);
+                if(pila!=NULL)
+                {
+                    cout<<v<<"|"<<endl;
+                }
+                else
+                {
+                    cout<<"*"<<endl;
+                }
+            }*/
+        }
+
+
+
         break;
+    default:
+        cout<<"La opción seleccionada no existe, por favor intente de nuevo"<<endl;
 
 
     }
 
-
+   }
 
 
 
