3159935b74925812dec3f9f479d128af9e76ef7a
IamLAM <ingeniero@luismonge.com.mx>
2018-11-07 00:18:12 -0800

    Estructura nodo  y declaración de función insertar

--- colas/main.cpp
+++ colas/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+struct Nodo{
+
+int dato
+Nodo *siguiente;
+};
+
+void insertarCola(Nodo *&frente,Nodo *&final,int v);
+
+
 int main()
 {
     cout << "Trabajando en colas" << endl;
     return 0;
+}
+
+void insertarCola(Nodo *&frente,Nodo *&final,int v){
+Nodo *insCola=new Nodo();
+
 }
