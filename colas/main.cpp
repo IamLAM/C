fc02d082d8787fe6e396de18121dba0f07dfd114
IamLAM <ingeniero@luismonge.com.mx>
2018-11-07 00:34:45 -0800

    Se comprobo que la cola estuviera vacia y  se mando a insertar un valor

--- colas/main.cpp
+++ colas/main.cpp
@@ -1,23 +1,56 @@
 #include <iostream>
+#include<conio.h>
+#include<stdlib.h>
 
 using namespace std;
 
-struct Nodo{
+struct Nodo
+{
 
-int dato
-Nodo *siguiente;
+    int dato;
+    Nodo *siguiente;
 };
 
 void insertarCola(Nodo *&frente,Nodo *&final,int v);
-
+bool colaVacia(Nodo *);
 
 int main()
 {
     cout << "Trabajando en colas" << endl;
+    Nodo *frente=NULL;
+    Nodo *fin=NULL;
+    int v;
+    cout<<"Proporciona un valor"<<endl;
+    cin>>v;
+
+
+
     return 0;
 }
 
-void insertarCola(Nodo *&frente,Nodo *&final,int v){
-Nodo *insCola=new Nodo();
+void insertarCola(Nodo *&frente,Nodo *&fin,int v)
+{
+    Nodo *insCola=new Nodo();
+    insCola->dato=v;
+    insCola->siguiente=NULL;
+    if(colaVacia(frente)==NULL)
+    {
+        frente=insCola;
+    }
+    else
+    {
 
+        fin->siguiente=insCola;
+
+    }
+    fin=insCola;
+
+}
+
+bool colaVacia(Nodo *frente)
+{
+    if(frente==NULL)
+        return true;
+    else
+        return false;
 }
