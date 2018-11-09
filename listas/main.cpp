c49e93442c7119e9df34209ba754f4d360b0f3f2
IamLAM <ingeniero@luismonge.com.mx>
2018-11-09 00:19:50 -0800

    Plantilla base para la declaración de listas

---
+++ listas/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Trabajando con listas(simples, doblemente enlazadas, simples circulares, circulares doblemente enlzadas)" << endl;
+    return 0;
+}
