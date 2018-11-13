8d4aefeeacb2f31eb319889884d728e01370fef2
IamLAM <ingeniero@luismonge.com.mx>
2018-11-12 23:50:52 -0800

    Declaración de función insertar nodo a la lista

--- listas/main.cpp
+++ listas/main.cpp
@@ -8,8 +8,23 @@ Nodo *siguiente;
 
 };
 
+void insertarLista(Nodo *&,int);
+
 int main()
 {
+    Nodo *lista=NULL;
+
+
     cout << "Trabajando con listas(simples, doblemente enlazadas, simples circulares, circulares doblemente enlzadas)" << endl;
     return 0;
+}
+
+void insertarLista(Nodo *&lista,int v){
+Nodo *insLista=new Nodo();
+insLista->valor=v;//asignamos el valor al nodo de la lista
+
+Nodo *aux=lista;
+Nodo *aux2;
+
+
 }
