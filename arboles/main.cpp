d400d78ac422779212b1517a903846c77c3d000e
IamLAM <ingeniero@luismonge.com.mx>
2018-12-17 23:36:55 -0800

    Estructura del nodo y función crear nodo lista

--- arboles/main.cpp
+++ arboles/main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+struct Nodo{
+
+int valor;
+Nodo *derecha;
+Nodo *izquierda;
+
+};
+
 int main()
 {
     cout << "Trabajando con arboles binarios" << endl;
     return 0;
+}
+
+Nodo *crearNodo(int v){
+    Nodo *nnodo=new Nodo();
+    Nodo->valor=v;
+    Nodo->derecha=NULL;
+    Nodo->izquierda=NULL;
+
+    return nnodo;
+
 }
