40bc556569a4cd1eff42c550a390ae15a0f170ec
IamLAM <ingeniero@luismonge.com.mx>
2018-12-17 23:49:58 -0800

    Función creada para insertar nodos, decidir izq o der

--- arboles/main.cpp
+++ arboles/main.cpp
@@ -2,11 +2,12 @@
 
 using namespace std;
 
-struct Nodo{
+struct Nodo
+{
 
-int valor;
-Nodo *derecha;
-Nodo *izquierda;
+    int valor;
+    Nodo *derecha;
+    Nodo *izquierda;
 
 };
 
@@ -16,12 +17,43 @@ int main()
     return 0;
 }
 
-Nodo *crearNodo(int v){
+Nodo *crearNodo(int v)
+{
     Nodo *nnodo=new Nodo();
-    Nodo->valor=v;
-    Nodo->derecha=NULL;
-    Nodo->izquierda=NULL;
+    nnodo->valor=v;
+    nnodo->derecha=NULL;
+    nnodo->izquierda=NULL;
 
     return nnodo;
+
+}
+
+void insertar(Nodo *&arbol,int v)
+{
+    int vRaiz;
+    if(arbol==NULL) //validamos que el arbol este vacio
+    {
+        Nodo *nnodo=crearNodo(v);
+        arbol=nnodo;
+    }
+    else
+    {
+        vRaiz=arbol->valor;
+        if(v<vRaiz)
+        {
+
+            insertar(arbol->izquierda,v);
+        }
+        else
+        {
+            insertar(arbol->derecha,v);
+
+        }
+
+
+    }
+
+
+
 
 }
